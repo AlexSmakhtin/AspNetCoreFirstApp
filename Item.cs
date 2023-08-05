e0fe762318b7d87d89be2f83df4e62f53f2c2047
Aleksey Smakhtin <lokomotiv96000@gmail.com>
2023-08-05 17:29:10 +0300

    dz

---
+++ Item.cs
@@ -0,0 +1,21 @@
+﻿namespace AspNetCoreFirstApp
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public Item(string name, decimal cost)
+        {
+            if (string.IsNullOrEmpty(name))
+                Name = string.Empty;
+            if (cost <= 0)
+                cost = 0;
+            Name = name;
+            Cost = cost;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - {Cost}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,48 +1,162 @@
 using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
 
 namespace AspNetCoreFirstApp
 {
     public class Program
     {
+        static List<Item> items = new List<Item>();
+        static object lockObject = new object();
         public static void Main(string[] args)
         {
+            items.Add(new Item("Item 1", 1.99m));
+            items.Add(new Item("Item 2", 2.99m));
+            items.Add(new Item("Item 3", 3.99m));
+            items.Add(new Item("Item 4", 4.99m));
+            items.Add(new Item("Item 5", 5.99m));
+            items.Add(new Item("Item 6", 6.99m));
+            items.Add(new Item("Item 7", 7.99m));
+            items.Add(new Item("Item 8", 8.99m));
+
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            //app.MapGet("/", () => DateTime.UtcNow.ToString() + " Дата на сегодня");
-            //app.MapGet("/new_year", () => Math.Round((DateTime.Parse($"01.01.{DateTime.Now.Year + 1}") - DateTime.Now)
-            //.TotalDays).ToString() + " дней осталось до Нового года");
+            //1.1 add item
+            app.MapPost("/items", AddItem); //REST
+            app.MapPost("/add_item", AddItem); //RPC
+            //1.2 get items
+            app.MapGet("/items", GetItems); //REST
+            app.MapGet("/items/{index}", GetItemByIndex);//REST by index
+            app.MapGet("/get_items", GetItems); //RPC
+            app.MapGet("/get_item", GetItemByIndex); //RPC by index
+            //1.3 delete item
+            app.MapDelete("/items/{index}", DeleteItem); //REST
+            app.MapPost("/delete_item", DeleteItem); //RPC
+            //1.4 update item
+            app.MapPut("/items/{index}", UpdateItem); //REST
+            app.MapPost("/update_item", UpdateItem); //RPC
+            //2 clear 
+            app.MapPost("/clear", ClearTheList);
+            app.Run();
+        }
 
-            //1
-            app.MapGet("/customs_duty", (double price) =>
+        static void ClearTheList(HttpContext context)
+        {
+            lock (lockObject)
             {
-                string message = "Итоговая стоимость посылки с пошлиной: ";
-                int limit = 200;
-                if (price <= limit)
-                    return message + price.ToString() + " евро";
-                return message + (price += price * 0.15) + " евро";
-            });
+                items.Clear();
+            }
+            return;
+        }
 
-            //2
-            app.MapGet("/date_time", (string language) =>
+        static async Task UpdateItem(HttpContext context, int index)
+        {
+            if (context.Request.HasJsonContentType())
             {
                 try
                 {
-                    DateTime now = DateTime.Now;
-                    CultureInfo culture = CultureInfo.GetCultureInfo(language);
-                    DateTimeFormatInfo dateTimeFormatInfo = culture.DateTimeFormat;
-
-                    return now.ToString(dateTimeFormatInfo.FullDateTimePattern, culture);
+                    var item = await context.Request.ReadFromJsonAsync<Item>();
+                    if (item != null &&
+                        item.Name != string.Empty &&
+                        item.Cost != 0)
+                    {
+                        lock (lockObject)
+                        {
+                            items[index] = item;
+                        }
+                        context.Response.StatusCode = StatusCodes.Status201Created;
+                        return;
+                    }
                 }
-                catch (CultureNotFoundException ex)
+                catch (Exception)
                 {
-
-                    return ex.Message;
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
                 }
-            });
-
-            app.Run();
+            }
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
         }
+
+        static void DeleteItem(HttpContext context, int index)
+        {
+            try
+            {
+                lock (lockObject)
+                {
+                    items.RemoveAt(index);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        static string GetItemByIndex(HttpContext context, int index)
+        {
+            try
+            {
+                lock (lockObject)
+                {
+                    return items[index].ToString();
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return $"Item with index \"{index}\" not found";
+            }
+        }
+
+        static async Task AddItem(HttpContext context)
+        {
+            if (context.Request.HasJsonContentType())
+            {
+                try
+                {
+                    var item = await context.Request.ReadFromJsonAsync<Item>();
+                    if (item != null &&
+                        item.Name != string.Empty &&
+                        item.Cost != 0)
+                    {
+                        lock (lockObject)
+                        {
+                            items.Add(item);
+                        }
+                        context.Response.StatusCode = StatusCodes.Status201Created;
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
+                }
+            }
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            return;
+        }
+
+        static string GetItems()
+        {
+            var stringBuilder = new StringBuilder();
+            lock (lockObject)
+            {
+                foreach (var item in items)
+                {
+                    stringBuilder.Append(item.ToString());
+                    stringBuilder.Append('\n');
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
     }
 }
