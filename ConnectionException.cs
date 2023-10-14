49db0d713614ae5f2c001298a86038f5ff0bc1cb
Aleksey Smakhtin <lokomotiv96000@gmail.com>
2023-10-14 17:16:30 +0300

    dz9

--- ConnectionException.cs
+++ ConnectionException.cs
@@ -1,7 +1,13 @@
-﻿namespace AspNetCoreFirstApp
+﻿using System.Runtime.Serialization;
+
+namespace AspNetCoreFirstApp
 {
     public class ConnectionException : Exception
     {
+        public ConnectionException() { }
+
         public ConnectionException(string message) : base(message) { }
+        public ConnectionException(string message, Exception inner) : base(message, inner) { }
+        protected ConnectionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
