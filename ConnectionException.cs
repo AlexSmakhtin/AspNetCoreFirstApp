5a13c1c79ece9c429903a345aa929bc5d8c5f064
Aleksey Smakhtin <lokomotiv96000@gmail.com>
2023-10-14 15:28:56 +0300

    dz8

---
+++ ConnectionException.cs
@@ -0,0 +1,18 @@
+﻿namespace AspNetCoreFirstApp
+{
+    public class ConnectionException : Exception
+    {
+        private readonly Exception _parent;
+        public ConnectionException(Exception parent)
+        {
+            _parent = parent;
+        }
+        public override string Message
+        {
+            get
+            {
+                return _parent.Message;
+            }
+        }
+    }
+}
---
+++ EmailSenderRetryDecorator.cs
@@ -0,0 +1,46 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace AspNetCoreFirstApp
+{
+
+
+    public class EmailSenderRetryDecorator : IEmailSender
+    {
+        private readonly IEmailSender _inner;
+        private readonly ILogger<EmailSenderRetryDecorator> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly AsyncRetryPolicy _policy;
+        public EmailSenderRetryDecorator(
+            IEmailSender inner,
+            ILogger<EmailSenderRetryDecorator> logger,
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _inner = inner;
+            _logger = logger;
+            _policy = Policy
+                .Handle<ConnectionException>()
+                .RetryAsync(_configuration
+                                .GetSection("SmtpConfig")
+                                .GetValue<int>("MaxRetryCount"),
+                           (ex, retryAttempt) =>
+                                _logger.LogWarning(ex, "Caught an error. Retrying: {attempt}", retryAttempt));
+        }
+        public async Task SendEmailAsync(
+            string fromName,
+            string fromEmail,
+            string toName,
+            string toEmail,
+            string subject,
+            string body,
+            CancellationToken token)
+        {
+            var result = await _policy
+                .ExecuteAndCaptureAsync(() => _inner.SendEmailAsync(
+                                            fromName, fromEmail, toName, toEmail, subject, body, token));
+            if (result.Outcome == OutcomeType.Failure)
+                throw result.FinalException;
+        }
+    }
+}
